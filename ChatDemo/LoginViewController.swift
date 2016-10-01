2516ed7ab5f39dceb090168efcf26d8b580b553f
Sriram Rajendran <sriramrajendrantkp@yahoo.in>
2016-10-01 16:39:09 +0530

    Part-2 LoginVC with UI completed

--- ChatDemo/LoginViewController.swift
+++ ChatDemo/LoginViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+  //MARK: outlets
   
   
+  @IBOutlet weak var loginAnonymOutlet: UIButton!
   
   
+  @IBOutlet weak var gooleBtnOutlet: UIButton!
   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      
+      self.loginAnonymOutlet.layer.borderWidth = 2.0
+      self.loginAnonymOutlet.layer.borderColor = UIColor.redColor().CGColor
+      self.loginAnonymOutlet.layer.cornerRadius = self.loginAnonymOutlet.frame.size.height/2
+      
+      
+      self.gooleBtnOutlet.layer.borderWidth = 2.0
+      self.gooleBtnOutlet.layer.borderColor = UIColor.redColor().CGColor
+      self.gooleBtnOutlet.layer.cornerRadius = self.gooleBtnOutlet.frame.size.height/2
+      
     }
 
     override func didReceiveMemoryWarning() {
